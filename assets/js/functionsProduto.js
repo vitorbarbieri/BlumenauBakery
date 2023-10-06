6f122ca6517fa7c973ad3d4e2d808749aad05c7a
vitorbarbieri <vbarbierisilva@gmail.com>
2023-10-06 16:27:53 -0300

    Inserir plugin Tinymce

--- assets/js/functionsProduto.js
+++ assets/js/functionsProduto.js
@@ -1,11 +1,30 @@
-function openModal()
-{
+// Inserir o plugin Tinymce
+$(document).on('focusin', function (e) {
+    if ($(e.target).closest(".tox-dialog").length) {
+        e.stopImmediatePropagation();
+    }
+});
+tinymce.init({
+    selector: '#txtDescricao',
+    width: "100%",
+    height: 400,
+    resize: false,
+    statubar: true,
+    plugins: [
+        "advlist autolink link image lists charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+        "save table contextmenu directionality emoticons template paste textcolor"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
+});
+
+function openModal() {
     // rowTable = "";
     // document.querySelector("#divBarCode").classList.add("notblock");
     // document.querySelector("#containerGallery").classList.add("notblock");
     // document.querySelector("#containerImages").innerHTML = "";
 
-    
+
     document.querySelector('#idProduto').value = "";
     document.querySelector('#titleModal').innerHTML = "Criar Usuário";
     document.querySelector('.modal-header').classList.replace("headerUpdate", "headerRegister");
--- view/partials/FooterAdmin.php
+++ view/partials/FooterAdmin.php
@@ -8,13 +8,14 @@
 <script src="<?= media() ?>/js/bootstrap.min.js"></script>
 <script src="<?= media() ?>/js/main.js"></script>
 <script src="<?= media() ?>/js/functionsAdmin.js"></script>
-<script src="<?= media(); ?>/js/<?= $data['page_functions_js'] ?>"></script>
 <!-- Font Awesome -->
 <script src="<?= media(); ?>/js/fontawesome.js"></script>
 <!-- The javascript plugin to display page loading on top-->
 <script src="<?= media() ?>/js/plugins/pace.min.js"></script>
 <!-- Sweet alert 2 - https://sweetalert2.github.io/ -->
 <script type="text/javascript" src="<?= media(); ?>/js/plugins/sweetalert2.all.min.js"></script>
+<!-- Tinymce - https://www.tiny.cloud/ -->
+<script type="text/javascript" src="<?= media(); ?>/js/tinymce/tinymce.min.js"></script>
 <!-- Bootstrap-select -->
 <script type="text/javascript" src="<?= media(); ?>/js/plugins/bootstrap-select.min.js"></script>
 <!-- Data table plugin -->
@@ -27,6 +28,7 @@
 <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
 
+<script src="<?= media(); ?>/js/<?= $data['page_functions_js'] ?>"></script>
 </body>
 
 </html>
