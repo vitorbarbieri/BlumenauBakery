76ab56728e0707e0b7eb9fbae44e907f96a80217
vitorbarbieri <vbarbierisilva@gmail.com>
2023-10-11 19:17:12 -0300

    Ajustar logo

--- view/partials/HeaderAdmin.php
+++ view/partials/HeaderAdmin.php
@@ -19,7 +19,10 @@
 
 <body class="app sidebar-mini">
     <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="<?= base_url() ?>/dashboard">Blumenau Bakery</a>
+    <header class="app-header">
+        <a class="app-header__logo" href="<?= base_url() ?>/dashboard">
+            <img src="<?= media(); ?>/img/logo.png" alt="Logo">
+        </a>
         <!-- Sidebar toggle button-->
         <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
         <!-- Navbar Right Menu-->
--- view/partials/HeaderLoja.php
+++ view/partials/HeaderLoja.php
@@ -5,21 +5,21 @@
     <title><?= $data['page_tag'] ?></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="<?= media() ?>/loja/img/icons/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/fonts/iconic/css/material-design-iconic-font.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/fonts/linearicons-v1.0.0/icon-font.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/vendor/animate/animate.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/vendor/css-hamburgers/hamburgers.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/vendor/animsition/css/animsition.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/vendor/select2/select2.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/vendor/daterangepicker/daterangepicker.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/vendor/slick/slick.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/vendor/MagnificPopup/magnific-popup.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/vendor/perfect-scrollbar/perfect-scrollbar.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/css/util.css">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/loja/css/main.css">
+    <link rel="icon" type="image/png" href="<?= media(); ?>/loja/img/icons/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/fonts/linearicons-v1.0.0/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/MagnificPopup/magnific-popup.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/css/util.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/css/main.css">
 </head>
 
 <body class="animsition">
@@ -32,24 +32,12 @@
             <div class="top-bar">
                 <div class="content-topbar flex-sb-m h-full container">
                     <div class="left-top-bar">
-                        Free shipping for standard order over $100
+                        Entrega grátis para compra acima de R$100,00
                     </div>
 
                     <div class="right-top-bar flex-w h-full">
                         <a href="#" class="flex-c-m trans-04 p-lr-25">
-                            Help & FAQs
-                        </a>
-
-                        <a href="#" class="flex-c-m trans-04 p-lr-25">
                             My Account
-                        </a>
-
-                        <a href="#" class="flex-c-m trans-04 p-lr-25">
-                            EN
-                        </a>
-
-                        <a href="#" class="flex-c-m trans-04 p-lr-25">
-                            USD
                         </a>
                     </div>
                 </div>
@@ -60,39 +48,23 @@
 
                     <!-- Logo desktop -->
                     <a href="#" class="logo">
-                        <img src="<?= media() ?>/loja/img/icons/logo.png" alt="Logo">
+                        <img src="<?= media(); ?>/loja/img/icons/logo.png" alt="Logo">
                     </a>
 
                     <!-- Menu desktop -->
                     <div class="menu-desktop">
                         <ul class="main-menu">
                             <li class="active-menu">
-                                <a href="index.html">Home</a>
-                                <ul class="sub-menu">
-                                    <li><a href="index.html">Homepage 1</a></li>
-                                    <li><a href="home-02.html">Homepage 2</a></li>
-                                    <li><a href="home-03.html">Homepage 3</a></li>
-                                </ul>
+                                <a href="<?= base_url(); ?>">Home</a>
                             </li>
-
                             <li>
-                                <a href="product.html">Shop</a>
+                                <a href="<?= base_url(); ?>/loja">Loja</a>
                             </li>
-
-                            <li class="label1" data-label1="hot">
-                                <a href="shoping-cart.html">Features</a>
-                            </li>
-
                             <li>
-                                <a href="blog.html">Blog</a>
+                                <a href="<?= base_url(); ?>/sobre">Sobre</a>
                             </li>
-
                             <li>
-                                <a href="about.html">About</a>
-                            </li>
-
-                            <li>
-                                <a href="contact.html">Contact</a>
+                                <a href="<?= base_url(); ?>/contato">Contato</a>
                             </li>
                         </ul>
                     </div>
@@ -103,13 +75,9 @@
                             <i class="zmdi zmdi-search"></i>
                         </div>
 
-                        <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="2">
+                        <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="10">
                             <i class="zmdi zmdi-shopping-cart"></i>
                         </div>
-
-                        <a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
-                            <i class="zmdi zmdi-favorite-outline"></i>
-                        </a>
                     </div>
                 </nav>
             </div>
@@ -119,7 +87,7 @@
         <div class="wrap-header-mobile">
             <!-- Logo moblie -->
             <div class="logo-mobile">
-                <a href="index.html"><img src="<?= media() ?>/loja/images/icons/logo-01.png" alt="IMG-LOGO"></a>
+                <a href="index.html"><img src="<?= media(); ?>/loja/images/icons/logo-01.png" alt="IMG-LOGO"></a>
             </div>
 
             <!-- Icon header -->
@@ -215,7 +183,7 @@
         <div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
             <div class="container-search-header">
                 <button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
-                    <img src="<?= media() ?>/loja/images/icons/icon-close2.png" alt="CLOSE">
+                    <img src="<?= media(); ?>/loja/images/icons/icon-close2.png" alt="CLOSE">
                 </button>
 
                 <form class="wrap-search-header flex-w p-l-15">
