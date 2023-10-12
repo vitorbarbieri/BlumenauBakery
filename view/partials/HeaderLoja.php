78d877006246bbe8750131213f6e29d08e120c08
vitorbarbieri <vbarbierisilva@gmail.com>
2023-10-12 10:17:42 -0300

    Configurar Modal e Seção produtos

--- view/partials/HeaderLoja.php
+++ view/partials/HeaderLoja.php
@@ -20,10 +20,10 @@
     <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/vendor/perfect-scrollbar/perfect-scrollbar.css">
     <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/css/util.css">
     <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/css/main.css">
+    <link rel="stylesheet" type="text/css" href="<?= media(); ?>/loja/css/style.css">
 </head>
 
 <body class="animsition">
-
     <!-- Header -->
     <header>
         <!-- Header desktop -->
@@ -34,7 +34,6 @@
                     <div class="left-top-bar">
                         Entrega grátis para compra acima de R$100,00
                     </div>
-
                     <div class="right-top-bar flex-w h-full">
                         <a href="#" class="flex-c-m trans-04 p-lr-25">
                             Minha Conta
@@ -45,15 +44,12 @@
                     </div>
                 </div>
             </div>
-
             <div class="wrap-menu-desktop">
                 <nav class="limiter-menu-desktop container">
-
                     <!-- Logo desktop -->
                     <a href="<?= base_url(); ?>" class="logo">
                         <img src="<?= media(); ?>/loja/img/icons/logo.png" alt="Logo">
                     </a>
-
                     <!-- Menu desktop -->
                     <div class="menu-desktop">
                         <ul class="main-menu">
@@ -71,7 +67,6 @@
                             </li>
                         </ul>
                     </div>
-
                     <!-- Icon header -->
                     <div class="wrap-icon-header flex-w flex-r-m">
                         <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
@@ -85,7 +80,6 @@
                 </nav>
             </div>
         </div>
-
         <!-- Header Mobile -->
         <div class="wrap-header-mobile">
             <!-- Logo moblie -->
@@ -94,7 +88,6 @@
                     <img src="<?= media(); ?>/loja/img/icons/logo.png" alt="Logo">
                 </a>
             </div>
-
             <!-- Icon header -->
             <div class="wrap-icon-header flex-w flex-r-m m-r-15">
                 <div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
@@ -104,7 +97,6 @@
                     <i class="zmdi zmdi-shopping-cart"></i>
                 </div>
             </div>
-
             <!-- Button show menu -->
             <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
                 <span class="hamburger-box">
@@ -112,8 +104,6 @@
                 </span>
             </div>
         </div>
-
-
         <!-- Menu Mobile -->
         <div class="menu-mobile">
             <ul class="topbar-mobile">
@@ -122,7 +112,6 @@
                         Entrega grátis para compra acima de R$100,00
                     </div>
                 </li>
-
                 <li>
                     <div class="right-top-bar flex-w h-full">
                         <a href="#" class="flex-c-m p-lr-10 trans-04">
@@ -134,7 +123,6 @@
                     </div>
                 </li>
             </ul>
-
             <ul class="main-menu-m">
                 <li>
                     <a href="<?= base_url(); ?>">Home</a>
@@ -150,14 +138,12 @@
                 </li>
             </ul>
         </div>
-
         <!-- Modal Search -->
         <div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
             <div class="container-search-header">
                 <button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
                     <img src="<?= media(); ?>/loja/images/icons/icon-close2.png" alt="CLOSE">
                 </button>
-
                 <form class="wrap-search-header flex-w p-l-15">
                     <button class="flex-c-m trans-04">
                         <i class="zmdi zmdi-search"></i>
